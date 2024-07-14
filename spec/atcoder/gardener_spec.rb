3c6c61b2f9cacb885a526dd869185e34d88823c0
Masaya Ohashi <m38.ohashi@gmail.com>
2024-07-14 19:05:43 +0900

    initial commit

---
+++ spec/atcoder/gardener_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Atcoder::Gardener do
+  it "has a version number" do
+    expect(Atcoder::Gardener::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
